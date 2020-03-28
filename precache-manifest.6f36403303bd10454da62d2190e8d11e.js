e18fe2d02eeaaaa58540e91e4d6687f3d0a05b81
Evan Slawski <evan.slawski@ithaka.org>
2020-03-28 14:02:57 -0400

    Updates

---
+++ precache-manifest.6f36403303bd10454da62d2190e8d11e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5d5a43acc84ae92209ddf45b35bfa27",
+    "url": "/where-is-waldo/index.html"
+  },
+  {
+    "revision": "e06fa5fa4cd7a04d88af",
+    "url": "/where-is-waldo/static/css/main.8b86d546.chunk.css"
+  },
+  {
+    "revision": "655519b0b55f41574058",
+    "url": "/where-is-waldo/static/js/2.b0c38a7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/where-is-waldo/static/js/2.b0c38a7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06fa5fa4cd7a04d88af",
+    "url": "/where-is-waldo/static/js/main.3e123f72.chunk.js"
+  },
+  {
+    "revision": "ae8483e314c4a2d64902",
+    "url": "/where-is-waldo/static/js/runtime-main.cb8a3afc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.384bfde1d5d640f5fef04a9748b5d845.js"
+  "/where-is-waldo/precache-manifest.6f36403303bd10454da62d2190e8d11e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/where-is-waldo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
